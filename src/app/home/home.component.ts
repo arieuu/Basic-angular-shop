50dfadca4424f89e2f8a6e19787c9000c760c04b
Pliskin <arieljanickcarvalho@gmail.com>
2024-07-30 16:48:19 +0100

    Added working pagination and brought project to a PRESENTABLE state

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { ProductsService } from '../services/products.service';
 import { Product, Products } from '../../types';
 import { ProductComponent } from '../components/product/product.component';
 import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent, CommonModule],
+  imports: [ProductComponent, CommonModule, PaginatorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -15,16 +16,28 @@ export class HomeComponent {
   constructor(private productsService: ProductsService) {}
 
   products: Product[] = [];
+  
+  totalRecords: number = 0;
+  rows: number = 5;
 
   onProductOutput(product: Product) {
     console.log(product, "Output");
   }
 
-  ngOnInit() {
-    this.productsService.getProducts("http://localhost:3000/clothes", {page: 0, perPage: 5})
+  onPageChange(event: any) {
+    this.fetchProducts(event.page, event.rows);
+  }
+
+  fetchProducts(page: number, perPage: number) {
+    this.productsService.getProducts("http://localhost:3000/clothes", { page, perPage })
     .subscribe((products: Products) => {
       // console.log(products.items)
       this.products = products.items
+      this.totalRecords = products.total;
     })
+  }
+
+  ngOnInit() {
+    this.fetchProducts(0, this.rows);
   }
 }
